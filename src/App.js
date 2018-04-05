2ad7a5d92949a97fb8d62b4d14c2f8121791c2ce
m <mn@domain.com>
2018-04-05 17:13:22 +0200

    materialize

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {Button, Icon, Row, Col} from 'react-materialize';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    return (<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
 
-          This is exciting !
-        </p>
-      </div>
-    );
+        <Button waves='light'>
+          <Icon>thumb_up</Icon>
+        </Button>
+
+      </p>
+    </div>);
   }
 }
 
