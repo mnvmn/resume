127a0ac42806c4aae3f48b0e89eae9e6e15268ab
m <mn@domain.com>
2018-04-05 17:31:52 +0200

    btn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import {Button, Icon, Row, Col} from 'react-materialize';
+import {Parallax, Button, Icon, Row, Col} from 'react-materialize';
 import './App.css';
 
 class App extends Component {
   render() {
     return (<div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
+      </header> */
+      }
 
-        <Button waves='light'>
-          <Icon>thumb_up</Icon>
-        </Button>
+      <div>
+        <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/>
+        <div className="section white">
+          <div className="row container">
 
-      </p>
+            <h2 className="header">CV</h2>
+
+            <Button className="red lighten-2" waves='light' title="translate">
+              {/* <Icon>swap_vert</Icon> */}
+              {/* <Icon>low_priority</Icon> */}
+              Translate
+              <Icon left>subject</Icon>
+              
+            </Button>
+          </div>
+        </div>
+        <Parallax imageSrc="http://materializecss.com/images/parallax2.jpg"/>
+      </div>
+
     </div>);
   }
 }
---
+++ src/Cv.js
@@ -0,0 +1,8 @@
+
+{/* <Icon>swap_vert</Icon> */
+} {/* <Icon>low_priority</Icon> */
+} {/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo"/>
+  <h1 className="App-title">Welcome to React</h1>
+</header> */
+}
