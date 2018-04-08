e75ea6befe7100473697162413031eed2c7937fe
Unknown <matus.nevedel@gmail.com>
2018-04-08 16:32:52 +0200

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Cv from './Cv';
 import logo from './logo.svg';
 import './App.css';
+import('bootstrap')
 
 class App extends Component {
   render() {
