5219cd7e294eb3f550cd0692713a6b957eb8f86b
Unknown <matus.nevedel@gmail.com>
2018-04-09 00:14:26 +0200

    util

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,22 +4,21 @@ import {Container, Col, Row} from 'reactstrap';
 class Footer extends React.Component {
   render() {
     const version = React.version
-    return (<footer className="footer">
+    return (<footer id="footer" className="d-flex align-items-center">
+
+      <div id="bootstrap-grid-util" className="position-absolute">
+        <span className="d-sm-none">XS</span>
+        <span className="d-none d-sm-block d-md-none">SM</span>
+        <span className="d-none d-md-block d-lg-none">MD</span>
+        <span className="d-none d-lg-block d-xl-none">LG</span>
+        <span className="d-none d-xl-block">XL</span>
+      </div>
+
       <Container>
         <Row>
-          <Col className="text-sm text-secondary">
-            <div id="bootstrap-grid-util">
-              <span className="d-sm-none">XS</span>
-              <span className="d-none d-sm-block d-md-none">SM</span>
-              <span className="d-none d-md-block d-lg-none">MD</span>
-              <span className="d-none d-lg-block d-xl-none">LG</span>
-              <span className="d-none d-xl-block">XL</span>
-            </div>
-          </Col>
-
           <Col className="text-sm text-right text-secondary">
             <small>
-              <span className="text-muted">powered by</span>
+              <span className="text-muted">#</span>
               {' '}
               <b>React v{version}</b>
             </small>
