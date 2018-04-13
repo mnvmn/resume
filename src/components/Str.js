28f9b0ef7b708b3cfc7da51e060324d4af108e70
Unknown <matus.nevedel@gmail.com>
2018-04-13 22:31:53 +0200

    links parsing

--- src/components/Str.js
+++ src/components/Str.js
@@ -4,7 +4,47 @@ class Str extends React.Component {
 
   render() {
     const lang = this.props.lang;
-    return (<span>{this.props.msg && this.props.msg[lang] ? this.props.msg[lang] : this.props.msg}</span>)
+    const msg = this.props.msg;
+    const trans = msg && msg[lang]
+      ? msg[lang]
+      : msg;
+
+    const finalText = trans
+      ? this.findLinks(trans)
+      : ''
+
+    return (<div>{finalText}</div>)
+  }
+
+  findLinks(str) {
+    const that = this
+    const links = str.match(/www\.[\w\.]*/g);
+    links && console.log(links);
+
+    let initialIndex = 0
+    if (links && links.length > 0) {
+      let buffer = links.reduce(function(buff, link) {
+        const index = str.indexOf(link);
+        const text = str.substr(initialIndex, index);
+
+        console.log(text.length, text)
+        console.log(link.length, link)
+        initialIndex += (text.length + link.length);
+
+        buff.push(<span key={Math.random()}>{text}</span>);
+        buff.push(<a key={Math.random()} href={link}>{link}</a>);
+
+        return buff;
+      }, [])
+
+      return buffer
+    } else {
+      return str;
+    }
+  }
+
+  makeLink(substr) {
+    return <a href={substr}>substr</a>
   }
 }
 
