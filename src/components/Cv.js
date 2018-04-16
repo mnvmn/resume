6b26a17ff41e5f5abe191903d2d6f486257ba38a
Unknown <matus.nevedel@gmail.com>
2018-04-17 01:37:23 +0200

    lint

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Container} from 'reactstrap';
-import TableInfo from './TableInfo.js';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Container } from 'reactstrap';
+import TableInfo from './TableInfo';
 import msgs from './../translations.json';
-import dispatcher from './../store/dispatchers.js';
-import reducer from './../store/reducers.js';
-
+import dispatcher from './../store/dispatchers';
+import reducer from './../store/reducers';
 
 class Cv extends React.Component {
   render() {
-    const data = this.props.data;
-    return (<div className="cv-container">
-      <Container>
-        <h3 className="text-center text-dark mt-2 mb-4">CV</h3>
-        <TableInfo data={data.info} isAsc={true}/>
-        <TableInfo data={data.education} title={msgs.title.education} isAsc={this.props.isAsc}/>
-        <TableInfo data={data.work} title={msgs.title.work} isAsc={this.props.isAsc}/>
-      </Container>
-    </div>)
+    const { data } = this.props;
+    return (
+      <div className="cv-container">
+        <Container>
+          <h3 className="text-center text-dark mt-2 mb-4">CV</h3>
+          <TableInfo data={data.info} isAsc={true} />
+          <TableInfo data={data.education} title={msgs.title.education} isAsc={this.props.isAsc} />
+          <TableInfo data={data.work} title={msgs.title.work} isAsc={this.props.isAsc} />
+        </Container>
+      </div>
+    );
   }
 }
 
-export default connect(reducer, dispatcher)(Cv)
+Cv.propTypes = {
+  data: PropTypes.object.isRequired,
+  isAsc: PropTypes.boolean.isRequired,
+};
+
+export default connect(reducer, dispatcher)(Cv);
