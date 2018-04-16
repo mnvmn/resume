d5db6718f7d248acbf9554f4b46eec3a45ac7fc5
Unknown <matus.nevedel@gmail.com>
2018-04-17 01:32:48 +0200

    lint

--- src/components/ControlFlipPage.js
+++ src/components/ControlFlipPage.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {Button} from 'reactstrap';
-import * as FontAwesome from 'react-icons/lib/fa'
+import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
+import * as FontAwesome from 'react-icons/lib/fa';
 
 class ControlFlipPage extends React.Component {
-
   render() {
-    return (<div className={'control-flipPage-' + this.props.orientation.toString()}>
-      <Button color="context" size="md" className="btn-vertical d-flex align-items-center">
-        {this.props.orientation === 'right' && <FontAwesome.FaAngleRight/>}
-        {this.props.orientation === 'left' && <FontAwesome.FaAngleLeft/>}
-      </Button>
-    </div>)
+    return (
+      <div className={`control-flipPage-${this.props.orientation.toString()}`}>
+        <Button color="context" size="md" className="btn-vertical d-flex align-items-center">
+          {this.props.orientation === 'right' && <FontAwesome.FaAngleRight />}
+          {this.props.orientation === 'left' && <FontAwesome.FaAngleLeft />}
+        </Button>
+      </div>
+    );
   }
 }
 
-export default ControlFlipPage
+ControlFlipPage.propTypes = {
+  orientation: PropTypes.boolean,
+};
+
+export default ControlFlipPage;
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {ButtonGroup, Button} from 'reactstrap';
+import { connect } from 'react-redux';
+import { ButtonGroup, Button } from 'reactstrap';
 import PropTypes from 'prop-types';
 import * as FontAwesome from 'react-icons/lib/fa';
 import msgs from './../translations.json';
@@ -11,49 +11,71 @@ import reducer from './../store/reducers';
 class Controls extends React.Component {
   render() {
     // const isAsc = this.props.isAsc;
-    const {isAsc} = this.props;
-    const groupClass = `d-flex${this.props.isVertical
-      ? ' btn-group-vertical rounded-0'
-      : ' ml-auto justify-content-end'}`;
-    const btnClass = `d-flex align-items-center${this.props.isVertical
-      ? ' btn-block rounded-0'
-      : ''}`;
+    const { isAsc } = this.props;
+    const groupClass = `d-flex${
+      this.props.isVertical ? ' btn-group-vertical rounded-0' : ' ml-auto justify-content-end'
+    }`;
+    const btnClass = `d-flex align-items-center${
+      this.props.isVertical ? ' btn-block rounded-0' : ''
+    }`;
 
-    return (<ButtonGroup className={groupClass}>
-      <Button color="primary" outline={true} size="xs" className={btnClass} href="https://github.com/mnvmn/resume" target="_blank">
-        <FontAwesome.FaCodeFork/>
-        &nbsp;&nbsp;<Str msg={msgs.btns.viewSource}/>
-      </Button>
-      <Button color="primary" outline={true} size="xs" className={btnClass} href="https://github.com/mnvmn/mnvmn.github.io/raw/master/CV_MN.pdf" target="_blank">
-        <FontAwesome.FaDownload/>
-        &nbsp;&nbsp;<Str msg={msgs.btns.downloadPdf}/>
-      </Button>
-      <Button color="primary" outline={true} size="xs" className={btnClass} onClick={this.props.toggleOrder}>
-        <span className="">
-          {
-            isAsc
-              ? <FontAwesome.FaSortAmountDesc/>
-              : <FontAwesome.FaSortAmountAsc/>
-          }
-        </span>
-        &nbsp;&nbsp; {
-          isAsc
-            ? <Str msg={msgs.btns.orderByDateDesc}/>
-            : <Str msg={msgs.btns.orderByDateAsc}/>
-        }
-      </Button>
-      <Button color="primary" outline={true} size="xs" className={btnClass} onClick={this.props.toggleLang}>
-        <FontAwesome.FaLanguage/>
-        &nbsp;&nbsp;<Str msg={msgs.btns.changeLanguage}/>
-      </Button>
-    </ButtonGroup>);
+    return (
+      <ButtonGroup className={groupClass}>
+        <Button
+          color="primary"
+          outline={true}
+          size="xs"
+          className={btnClass}
+          href="https://github.com/mnvmn/resume"
+          target="_blank"
+        >
+          <FontAwesome.FaCodeFork />
+          &nbsp;&nbsp;<Str msg={msgs.btns.viewSource} />
+        </Button>
+        <Button
+          color="primary"
+          outline={true}
+          size="xs"
+          className={btnClass}
+          href="https://github.com/mnvmn/mnvmn.github.io/raw/master/CV_MN.pdf"
+          target="_blank"
+        >
+          <FontAwesome.FaDownload />
+          &nbsp;&nbsp;<Str msg={msgs.btns.downloadPdf} />
+        </Button>
+        <Button
+          color="primary"
+          outline={true}
+          size="xs"
+          className={btnClass}
+          onClick={this.props.toggleOrder}
+        >
+          <span className="">
+            {isAsc ? <FontAwesome.FaSortAmountDesc /> : <FontAwesome.FaSortAmountAsc />}
+          </span>
+          &nbsp;&nbsp;{' '}
+          {isAsc ? <Str msg={msgs.btns.orderByDateDesc} /> : <Str msg={msgs.btns.orderByDateAsc} />}
+        </Button>
+        <Button
+          color="primary"
+          outline={true}
+          size="xs"
+          className={btnClass}
+          onClick={this.props.toggleLang}
+        >
+          <FontAwesome.FaLanguage />
+          &nbsp;&nbsp;<Str msg={msgs.btns.changeLanguage} />
+        </Button>
+      </ButtonGroup>
+    );
   }
 }
 
 Controls.propTypes = {
   isAsc: PropTypes.boolean,
+  isVertical: PropTypes.boolean,
   toggleLang: PropTypes.boolean,
-  toggleOrder: PropTypes.boolean
+  toggleOrder: PropTypes.boolean,
 };
 
 export default connect(reducer, dispatcher)(Controls);
