e09adbae94b9cd6780ee150a8a93544f05ca1b95
Unknown <matus.nevedel@gmail.com>
2018-04-19 03:33:40 +0200

    refactoring

--- src/pages/PagePoster.js
+++ src/pages/PagePoster.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Container } from 'reactstrap';
+import { Container, Col, Row } from 'reactstrap';
 import PropTypes from 'prop-types';
 import dispatcher from './../store/dispatchers';
 import reducer from './../store/reducers';
 import Logo from './../components/Logo';
-import TableInfo from './../components/TableInfo';
 import ControlFlipPage from './../components/ControlFlipPage';
+import Str from './../components/Str';
 
 class PagePoster extends React.Component {
   render() {
@@ -23,7 +23,16 @@ class PagePoster extends React.Component {
 
         <Container style={{ marginTop: '60px' }}>
           <h3 className="text-center text-dark mt-2 mb-4">CONTACT</h3>
-          <TableInfo data={data.contact} />
+          {data.contact.map(info => (
+            <Row key={Math.random()}>
+              <Col xs="5" lg="4" className="text-dark">
+                <Str msg={info.label} />
+              </Col>
+              <Col xs="7" lg="8" className="text-dark">
+                <Str msg={info.value} />
+              </Col>
+            </Row>
+          ))}
         </Container>
       </div>
     );
