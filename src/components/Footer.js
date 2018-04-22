d713af34ae601bc2e825567ba69ed6014113564d
Unknown <matus.nevedel@gmail.com>
2018-04-22 23:31:59 +0200

    Grid util, btn spread

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,14 +31,6 @@ class Footer extends React.Component {
     return (
       <footer id="footer" className="mt-auto">
         <div className="d-flex align-items-center">
-          {/* <div id="bootstrap-grid-util" className="position-absolute">
-          <span className="d-sm-none">XS</span>
-          <span className="d-none d-sm-block d-md-none">SM</span>
-          <span className="d-none d-md-block d-lg-none">MD</span>
-          <span className="d-none d-lg-block d-xl-none">LG</span>
-          <span className="d-none d-xl-block">XL</span>
-        </div> */}
-
           <Container>
             <Row>
               {this.state.repo.pushed_at && (
---
+++ src/components/GridUtil.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class GridUtil extends React.Component {
+  render() {
+    return (
+      <div id="bootstrap-grid-util" className="position-absolute">
+        <span className="d-sm-none">XS</span>
+        <span className="d-none d-sm-block d-md-none">SM</span>
+        <span className="d-none d-md-block d-lg-none">MD</span>
+        <span className="d-none d-lg-block d-xl-none">LG</span>
+        <span className="d-none d-xl-block">XL</span>
+      </div>
+    );
+  }
+}
+
+export default GridUtil;
