25bc4eebcaaef638e433b48f0169a28ad37b17d2
Unknown <matus.nevedel@gmail.com>
2018-05-08 20:20:08 +0200

    revert logo

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -8,10 +8,11 @@ class Animation extends Component {
   componentDidMount() {
     this.scene = Scene.init(this.props.width, this.props.height);
     this.el.appendChild(this.scene.renderer.domElement);
+
     const OrbitControls = orbitControls(THREE);
     const controls = new OrbitControls(this.scene.camera, this.el); // eslint-disable-line no-new
     controls.rotateSpeed = -1;
-    controls.enableKeys = false;
+
     this.start();
   }
 
@@ -54,8 +55,8 @@ Animation.propTypes = {
 };
 
 Animation.defaultProps = {
-  width: 400,
-  height: 400,
+  width: 200,
+  height: 200,
 };
 
 export default Animation;
--- src/movie/Scene.js
+++ src/movie/Scene.js
@@ -2,77 +2,44 @@ import * as THREE from 'three';
 
 const Scene = {
   init(width, height) {
-    this.setUpScene(width, height);
-    this.setUpGeometry();
-    return this;
-  },
-  setUpGeometry() {
+    const scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(15, width / height, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+
     // const geo1 = new THREE.BoxGeometry(10, 10, 10);
+    const geo2 = new THREE.TorusGeometry(10, 3, 16, 100);
+
     // const mat1 = new THREE.MeshBasicMaterial({ color: '#0475dc', wireframe: true });
     const mat2 = new THREE.LineBasicMaterial({ color: '#0475dc', linewidth: 1 });
     // const mat3 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 
-    // const thorus = new THREE.TorusGeometry(40, 1, 2, 50);
-    // new THREE.LineSegments(new THREE.EdgesGeometry(thorus), mat2),
+    const edges = new THREE.EdgesGeometry(geo2);
+    this.object = new THREE.LineSegments(edges, mat2);
 
-    const circle = new THREE.CircleGeometry(30, 32);
-    this.circles = [
-      new THREE.LineSegments(new THREE.EdgesGeometry(circle), mat2),
-      new THREE.LineSegments(new THREE.EdgesGeometry(circle.clone()), mat2),
-      new THREE.LineSegments(new THREE.EdgesGeometry(circle.clone()), mat2),
-    ];
+    renderer.setClearColor('#FFF');
+    renderer.setSize(width, height);
 
-    const sphere1 = new THREE.SphereGeometry(5, 10, 10);
-    const sphere2 = sphere1.clone();
-    const sphere3 = sphere1.clone();
-    this.spheres = [
-      new THREE.LineSegments(new THREE.EdgesGeometry(sphere1), mat2),
-      new THREE.LineSegments(new THREE.EdgesGeometry(sphere2), mat2),
-      new THREE.LineSegments(new THREE.EdgesGeometry(sphere3), mat2),
-    ];
+    this.scene = scene;
 
-    this.setUpObjectsPosition();
+    this.renderer = renderer;
 
-    this.spheres.concat(this.circles).forEach((el) => {
-      this.scene.add(el);
-    });
+    this.setUpCamera();
+    this.setUpPosition();
 
-    // this.render.frame();
+    scene.add(this.object);
+
+    return this;
   },
-  setUpScene(width, height) {
-    this.scene = new THREE.Scene();
-
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setClearColor('#FFF');
-    this.renderer.setSize(width, height);
-
-    this.camera = new THREE.PerspectiveCamera(40, width / height, 0.1, 1000);
+  setUpCamera() {
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-    this.camera.position.x = 20;
-    this.camera.position.y = 100;
-    this.camera.position.z = 40;
-
-    const axesHelper = new THREE.AxesHelper(5);
-    this.scene.add(axesHelper);
-
-    // controls.enableZoom = false;
+    this.camera.position.z = 100;
   },
-  setUpObjectsPosition() {
-    //  this.circles[0].rotation.x = Math.PI / 40 * 12;
-    this.circles[0].rotation.y = 0;
-    this.circles[1].rotation.y = Math.PI / 3;
-    this.circles[2].rotation.y = Math.PI / 3 * 2;
-
-    // this.spheres[0].position = new THREE.Vector3(0, 1, 0);
-    this.spheres[0].translateX(5);
-    // this.spheres[0].translate(0, 0, 10);
-    this.spheres[1].translateOnAxis(new THREE.Vector3(0, 1, 0), 5);
-    // this.spheres[2].translate(10, 10, 0);
+  setUpPosition() {
+    this.object.rotation.x = (Math.PI / 40) * 12;
   },
   animate() {
-    // this.pivots[0].rotation.z -= 0.002;
-    this.circles[1].rotation.z -= 0.002;
-    // this.spheres[1].translateOnAxis(new THREE.Vector3(0, 1, 0), 1);
+    this.object.rotation.z -= 0.002;
     // console.log(this.object.rotation.y);
   },
   renderFrame() {
